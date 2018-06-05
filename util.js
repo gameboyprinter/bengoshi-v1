1f23e12e4a8dfd62db0e456650c924f3b01e7d59
gameboyprinter <gameboyprinter@users.noreply.github.org>
2018-06-05 18:42:26 -0500

    npm stuff, version bump

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 // Just a few supporting functions to reduce code redundancy
 const fs = require("fs");
-const softVersion = "1.0rc1";
+const softVersion = "1.0.0";
 var clients = [];
 
 // Handles old AO1.X "encryption"
